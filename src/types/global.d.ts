7f9c91d584cc50e2d46aa58b281710de45251fe0
Abner <abnerchoba@hotmail.com>
2023-07-18 13:48:47 -0300

    global types and Book type

---
+++ src/types/global.d.ts
@@ -0,0 +1,14 @@
+type Book = {
+  title: string,
+  rank: number,
+  amazon_url: string,
+  description: string,
+  contributor: string,
+  author: string,
+  contributor_note: string,
+  price: string,
+  age_group: string,
+  publisher: string,
+  primary_isbn13: string,
+  primary_isbn10: string
+}
