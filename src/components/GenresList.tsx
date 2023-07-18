953583b848a1dc111ff5922dded662a0f09956ab
Abner <abnerchoba@hotmail.com>
2023-07-18 13:49:17 -0300

    add key to Genre

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -3,7 +3,7 @@ function GenreList() {
   return (
     <div className="flex flex-row flex-wrap gap-3 mx-40">
       {bookGenres.map(genre => (
-        <span className="genre-box">
+        <span className="genre-box" key={genre}>
           {genre}
         </span>
       ))}
